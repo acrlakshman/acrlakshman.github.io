37f3bf3df40edca508f6f4c3e77128b121f97af6
Lakshman Anumolu <anumolu@uwalumni.com>
2020-10-20 18:35:07 -0500

    added card to projects section

--- src/components/themes/basic/SectionProjects.tsx
+++ src/components/themes/basic/SectionProjects.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import SectionLabel from './SectionLabel';
 import Content from './Content';
 import re_weburl from '../../../auxiliaries/regex-weburl';
 import config from '../../../config';
-import Markdown from './Markdown';
+import Card from './Card';
 
 import { Project, Projects } from '../../../types/profileWeb';
 import {ProfileField} from '../../../types/fields'
@@ -15,52 +14,6 @@ interface Props {
   sectionDetail: Projects;
 }
 
-const renderTitle = (projectDetail: Project) => {
-  console.log(projectDetail);
-  if (projectDetail.webPage) {
-    if (
-      projectDetail.webPage.slug &&
-      projectDetail.webPage.detail &&
-      projectDetail.webPage.slug !== '' &&
-      projectDetail.webPage.detail !== ''
-    ) {
-      if (!re_weburl.test(projectDetail.webPage.slug)) {
-        return (
-          <Link to={`/project/${projectDetail.webPage.slug}`}>
-            {projectDetail.name}
-          </Link>
-        );
-      } else {
-        return (
-          <a href={projectDetail.webPage.slug}>{projectDetail.name}</a>
-        )
-      }
-    }
-  }
-
-  if (projectDetail.url && projectDetail.url !== '') {
-    return (
-      <a href={projectDetail.url}>{projectDetail.name}</a>
-    )
-  }
-
-  return (
-      projectDetail.name
-    )
-}
-
-const renderTeam = (projectDetail: Project): JSX.Element | undefined => {
-  if (projectDetail.team) {
-    return <Markdown className="content-text" content={projectDetail.team} />;
-  }
-}
-
-const renderDescription = (projectDetail: Project): JSX.Element => {
-  return (
-    <Markdown className="content-text" content={projectDetail.description} />
-  );
-}
-
 const renderProject = (projectDetail: Project, key: string): JSX.Element => {
   let imageSrc = projectDetail.thumbnail;
   if (imageSrc && !re_weburl.test(imageSrc)) {
@@ -68,23 +21,16 @@ const renderProject = (projectDetail: Project, key: string): JSX.Element => {
   }
 
   return (
-    <div
-      key={key}
-      className="flex-shrink-1 justify-content-start align-items-start align-content-start align-self-start flex-nowrap row align-content-start border content-item"
-    >
-      <div className="col-md-2 content-card-img">
-        <img
-          className="img-fluid border rounded image-item"
-          alt="thumbnail"
-          src={imageSrc}
-        />
-      </div>
-      <div className="col-md-10">
-        <div className="content-text-bold">{renderTitle(projectDetail)}</div>
-        {renderTeam(projectDetail)}
-        {renderDescription(projectDetail)}
-      </div>
-    </div>
+    <Card
+      id={key}
+      slugPrefix={`/project`}
+      url={projectDetail.url}
+      webPage={projectDetail.webPage}
+      title={projectDetail.name}
+      team={projectDetail.team}
+      description={projectDetail.description}
+      thumbnail={projectDetail.thumbnail}
+    />
   );
 };
 

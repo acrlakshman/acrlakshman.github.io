c98e92cb0c3017e70fd04ba7b76cfe9779a9a393
Lakshman Anumolu <anumolu@uwalumni.com>
2020-11-05 23:32:14 -0600

    update

---
+++ resume/go.mod
@@ -0,0 +1,5 @@
+module acrlakshman/profileio/resume
+
+go 1.14
+
+require github.com/acrlakshman/profileio-resume v0.0.0-20201106041541-6a963ef1d523
---
+++ resume/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/acrlakshman/profileio-resume/profileio"
+)
+
+func main() {
+	jsonData, _ := ioutil.ReadFile("./profile_resume.json")
+	profileio.ProfileIO(jsonData)
+
+	app := "xelatex"
+	if !commandExists(app) {
+		app = "pdflatex"
+		if !commandExists(app) {
+			log.Fatal("Cannot compile resume.tex")
+		}
+	}
+
+	os.Chdir("./resume")
+	cmd := exec.Command(app, "resume.tex")
+	out, err := cmd.Output()
+	if err != nil {
+		log.Printf("%v; %v", string(out), err)
+	}
+}
+
+func commandExists(cmd string) bool {
+	_, err := exec.LookPath(cmd)
+
+	return err == nil
+}
